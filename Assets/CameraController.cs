2fc710878cc1402c251d67d2f332ead014548dcd
ryou <visualjupiter@gmail.com>
2018-04-22 22:56:57 +0900

    ズーム処理記述

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float zoomSpeed = 2.0f;
+
+    private Camera cameraComponent;
+
+	// Use this for initialization
+	void Start () {
+        this.cameraComponent = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        this.cameraComponent.orthographicSize -= scroll * this.zoomSpeed;
+	}
+}
